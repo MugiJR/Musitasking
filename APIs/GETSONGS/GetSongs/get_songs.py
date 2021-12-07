fa17f8069a5e4548058ccfe57c7d77b1e6a3ca84
MugiJR <immugi@yahoo.com>
2021-12-07 15:56:45 +0100

    removed db creds

--- APIs/GETSONGS/GetSongs/get_songs.py
+++ APIs/GETSONGS/GetSongs/get_songs.py
@@ -4,11 +4,10 @@ import json
 import logging
 import os
 
-def retrieve_songs(duration=300):
+def retrieve_songs(duration):
     try:
         logging.info("Reading DB settings...")
-        #my_app_setting_db_cred_value = os.environ["DB_CREDENTIALS"]
-        my_app_setting_db_cred_value = "Driver={SQL Server};Server=tcp:musitasking.database.windows.net,1433;Database=musitaskingdb;Uid=elte;Pwd=admin@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+        my_app_setting_db_cred_value = os.environ["DB_CREDENTIALS"]
         logging.info("Retrived Settings...")
         conn = pyodbc.connect(my_app_setting_db_cred_value)
         df = pd.read_sql_query('SELECT * FROM SONG ORDER BY DURATION', conn)
@@ -22,7 +21,7 @@ def retrieve_songs(duration=300):
         # IGNORE DURATION_CUMSUM COLUMN
         #filtered_df = filtered_df[['id','title','song_url','duration']]
         filtered_df = filtered_df[['song_url']]
-        print(filtered_df)
+        #print(filtered_df)
         result = filtered_df.to_json(orient="records")
         logging.info("Fetched result successfully")
         parsed = json.loads(result)
@@ -38,9 +37,6 @@ def retrieve_songs(duration=300):
         sqlstate = ex.args[1]
         logging.error(sqlstate)
         return(500,sqlstate)
-
-if __name__ == "__main__":
-    retrieve_songs(900)
 
 
 
